ed58b6d5b6e9d9efe8bad6d90373c60fb0d50681
FilipVolny <fivo24@seznam.cz>
2023-12-01 17:36:04 +0100

    add debug mode

--- src/rt004/Program.cs
+++ src/rt004/Program.cs
@@ -10,47 +10,56 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        
-        string inputDir = "Scenes";
-        Console.Write("Enter the config file name from the " + '"' + inputDir + '"' + " directory:\n");
-        
-        string? configFile = Console.ReadLine();
-        if (configFile is null) throw new ArgumentNullException();
+        bool db = true;
 
-        string configPath = inputDir + "/" + configFile;
-        if (!File.Exists(configPath))
+        if (db)
         {
-            Console.Write("Config file of that name does not exist.\nAbort\n");
-            throw new Exception("Config file '" + configFile + "' does not exist.");
+            TestScenes ts = new TestScenes();
+            ts.Test();
         }
-        
-        Scene scene = JsonParser.ParseJsonConfig(configPath);
-
-        FloatImage fi = new FloatImage(1200, 1200, 3); //todo: should parametrize into its own function
-        int px = 1;         //modifies pixel size 
-        int sampleSize = 1; //sample per pixel
-
-        Vector3d[,] grid = scene.Camera.ParallelRayCastHierarchy(scene, sampleSize, fi.Width / px, fi.Height / px);
-
-        for (int y = 0; y < fi.Height; y++)
+        else
         {
-            for (int x = 0; x < fi.Width; x++)
+            string inputDir = "Scenes";
+            Console.Write("Enter the config file name from the " + '"' + inputDir + '"' + " directory:\n");
+
+            string? configFile = Console.ReadLine();
+            if (configFile is null) throw new ArgumentNullException();
+
+            string configPath = inputDir + "/" + configFile;
+            if (!File.Exists(configPath))
             {
-                float[] color = new float[3];
-                color[0] = (float)grid[x / px, y / px].X;
-                color[1] = (float)grid[x / px, y / px].Y;
-                color[2] = (float)grid[x / px, y / px].Z;
-
-                fi.PutPixel(x, y, color);
+                Console.Write("Config file of that name does not exist.\nAbort\n");
+                throw new Exception("Config file '" + configFile + "' does not exist.");
             }
+
+            Scene scene = JsonParser.ParseJsonConfig(configPath);
+
+            FloatImage fi = new FloatImage(1200, 1200, 3); //todo: should parametrize into its own function
+            int px = 1;         //modifies pixel size 
+            int sampleSize = 1; //sample per pixel
+
+            Vector3d[,] grid = scene.Camera.ParallelRayCastHierarchy(scene, sampleSize, fi.Width / px, fi.Height / px);
+
+            for (int y = 0; y < fi.Height; y++)
+            {
+                for (int x = 0; x < fi.Width; x++)
+                {
+                    float[] color = new float[3];
+                    color[0] = (float)grid[x / px, y / px].X;
+                    color[1] = (float)grid[x / px, y / px].Y;
+                    color[2] = (float)grid[x / px, y / px].Z;
+
+                    fi.PutPixel(x, y, color);
+                }
+            }
+
+            string outputFile = configFile.Split('.')[0] + ".pfm";
+            string outputDir = "Output";
+
+            fi.SavePFM(outputDir + "/" + outputFile);
+
+            Console.WriteLine("HDR image is finished.");
+            Console.WriteLine("File was saved as " + '"' + outputFile + '"' + " in the " + '"' + outputDir + '"' + " directory.");
         }
-
-        string outputFile = configFile.Split('.')[0] + ".pfm";
-        string outputDir = "Output";
-
-        fi.SavePFM(outputDir + "/" + outputFile);
-        
-        Console.WriteLine("HDR image is finished.");
-        Console.WriteLine("File was saved as " + '"' + outputFile + '"' + " in the " + '"' + outputDir + '"' + " directory.");
     }
 }
