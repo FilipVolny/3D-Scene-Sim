9352e5848ac2a89495201dd3dd95e41ded1a029e
FilipVolny <fivo24@seznam.cz>
2023-12-21 12:15:48 +0100

    add ExtractTranslationMatrix Matrix4d extension

---
+++ src/rt004/Matrix4dExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Mathematics;
+
+namespace rt004
+{
+    public static class Matrix4dExtensions
+    {
+        public static Matrix4d ExtractTranslationMatrix(this Matrix4d matrix)
+        {
+            Matrix4d result = Matrix4d.Identity;
+            result.Column3 = matrix.Column3;
+            return result;
+        }
+
+    }
+}
